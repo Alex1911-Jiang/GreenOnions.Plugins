9dd5dbc8d61fd36373d304366279bd1023d29113
Alex1911 <tjxx_jgd@163.com>
2022-11-26 00:22:29 +0800

    游戏结束后公布答案和修复跨群答题问题

--- GreenOnions.GuessTheSong/Config.cs
+++ GreenOnions.GuessTheSong/Config.cs
@@ -6,6 +6,6 @@
         public Dictionary<long, string[]> MusicIDAndAnswers { get; set; } = new Dictionary<long, string[]>();
         public HashSet<string> SearchKeywords { get; set; } = new HashSet<string>();
         public string RightAnswerReply { get; set; } = "恭喜你，答对了";
-        public string TimeOutReplyReply { get; set; } = "真遗憾，没有人猜出正确曲名，下次加油哦！";
+        public string TimeOutReplyReply { get; set; } = "真遗憾，没有人猜出正确曲名，答案为：\"<歌曲名称>\"，下次加油哦！";
     }
 }
