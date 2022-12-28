278630bdb07792aa5e77fa4a75a9c893d669e505
Alex1911-Jiang <tjxx_jgd@163.com>
2022-12-28 21:39:49 +0800

    适配新版（魔改版）WebUI

---
+++ GreenOnions.NovelAiClient/Config.cs
@@ -0,0 +1,54 @@
+﻿namespace GreenOnions.NovelAiClient
+{
+    public class Config
+    {
+        /// <summary>
+        /// 命令
+        /// </summary>
+        public string? Cmd { get; set; } = "<机器人名称>画[图画][:：]";
+        /// <summary>
+        /// 连接的前端（Naifu/WebUI/CustomWebUI）
+        /// </summary>
+        public string? ConnectFrontEnd { get; set; } = "Naifu";
+        /// <summary>
+        /// 前端地址
+        /// </summary>
+        public string? URL { get; set; } = "http://127.0.0.1:6969/";
+        /// <summary>
+        /// WebUI 的 fn_index 参数
+        /// </summary>
+        public int fn_index { get; set; } = 13;
+        /// <summary>
+        /// 图片宽（不适用于魔改版本WebUI）
+        /// </summary>
+        public int ImageWidth { get; set; } = 512;
+        /// <summary>
+        /// 图片高（不适用于魔改版本WebUI）
+        /// </summary>
+        public int ImageHeight { get; set; } = 768;
+        /// <summary>
+        /// 默认添加提示词
+        /// </summary>
+        public string? DefaultPrompt { get; set; } = "masterpiece, best quality,";
+        /// <summary>
+        /// 默认屏蔽词
+        /// </summary>
+        public string? DefaultUndesired { get; set; } = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw, r18, r-18, nude, nipple, nipples, breast, breasts, pussy, vaginal, asshole, penis, testicle, testicles, sex,";
+        /// <summary>
+        /// 开始绘制提示
+        /// </summary>
+        public string? StartDrawMessage { get; set; } = "开始绘制";
+        /// <summary>
+        /// 绘制完成提示
+        /// </summary>
+        public string? DrawEndMessage { get; set; } = "绘制完毕，耗时：<耗时>秒";
+        /// <summary>
+        /// 绘制错误提示
+        /// </summary>
+        public string? DrawErrorMessage { get; set; } = "绘制错误(Ｔ▽Ｔ)";
+        /// <summary>
+        /// 撤回时间（秒）
+        /// </summary>
+        public int RevokeSecond { get; set; } = 30;
+    }
+}
--- GreenOnions.NovelAiClient/NovelHandler.cs
+++ GreenOnions.NovelAiClient/NovelHandler.cs
@@ -9,11 +9,11 @@ namespace GreenOnions.NovelAiClient
 {
     public class NovelHandler : IPlugin
     {
+        private string? _pluginPath;
         private Config? _config;
         private IBotConfig? _botConfig;
         private IGreenOnionsApi? _api;
         private string? _strCmd;
-        private string _errorMsg = "";
         public string Name => "NovelAi画图";
 
         public string Description => "NovelAi画图插件";
@@ -30,15 +30,26 @@ namespace GreenOnions.NovelAiClient
         public void OnConnected(long selfId, IGreenOnionsApi api)
         {
             _api = api;
-            if (string.IsNullOrWhiteSpace(_errorMsg))
+            string configPath = Path.Combine(_pluginPath!, "config.json");
+            if (File.Exists(configPath))
             {
-                _strCmd = _config!.Cmd;
+                _config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
+                if (_config is null)
+                {
+                    for (int i = 0; i < _botConfig!.AdminQQ.Count; i++)
+                        _api.SendFriendMessageAsync(_botConfig.AdminQQ.ToArray()[i], $"葱葱NovelAi画图插件读取配置文件失败，请检查json格式，文件路径为：{configPath}");
+                }
             }
             else
             {
-                for (int i = 0; i < _botConfig!.AdminQQ.Count; i++)
-                    _api.SendFriendMessageAsync(_botConfig.AdminQQ.ToArray()[i], _errorMsg);
+                //配置文件例子
+                _config = new Config();
+                string customDataPath = Path.Combine(_pluginPath!, "custom_data.txt");
+                if (!File.Exists(customDataPath))
+                    File.WriteAllText(customDataPath, "");
             }
+            File.WriteAllText(configPath!, JsonConvert.SerializeObject(_config, Formatting.Indented));
+            _strCmd = _config!.Cmd;
         }
 
         public void OnDisconnected()
@@ -48,42 +59,15 @@ namespace GreenOnions.NovelAiClient
 
         public void OnLoad(string pluginPath, IBotConfig botConfig)
         {
+            _pluginPath = pluginPath;
             _botConfig = botConfig;
-            string configPath = Path.Combine(pluginPath, "config.json");
-            if (File.Exists(configPath))
-            {
-                _config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath));
-                if (_config == null)
-                {
-                    _errorMsg = $"葱葱NovelAi画图插件读取配置文件失败，请检查json格式，文件路径为：{configPath}";
-                }
-            }
-            else
-            {
-                //配置文件例子
-                _config = new Config()
-                {
-                    Cmd = "<机器人名称>画[图画][:：]",
-                    ConnectFrontEnd = "Naifu",
-                    URL = "http://127.0.0.1:6969/",
-                    ImageWidth = 512,
-                    ImageHeight = 768,
-                    DefaultPrompt = "masterpiece, best quality,",
-                    DefaultUndesired = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, nsfw, r18, r-18, nude, nipple, nipples, breast, breasts, pussy, vaginal, asshole, penis, testicle, testicles, sex,",
-                    StartDrawMessage = "开始绘制",
-                    DrawEndMessage = "绘制完毕，耗时：<耗时>秒",
-                    DrawErrorMessage = "绘制错误(Ｔ▽Ｔ)",
-                    RevokeSecond = 30,
-                };
-                File.WriteAllText(configPath, JsonConvert.SerializeObject(_config));
-            }
         }
 
         public bool OnMessage(GreenOnionsMessages msgs, long? senderGroup, Action<GreenOnionsMessages> Response)
         {
-            if (_config != null)
+            if (_config is not null)
             {
-                if (msgs.FirstOrDefault() is GreenOnionsTextMessage msg && _strCmd != null)
+                if (msgs.FirstOrDefault() is GreenOnionsTextMessage msg && _strCmd is not null)
                 {
                     Regex regex = new Regex(_api!.ReplaceGreenOnionsStringTags(_strCmd));
                     if (regex.IsMatch(msg.Text))
@@ -95,29 +79,42 @@ namespace GreenOnions.NovelAiClient
                             Response(_config.StartDrawMessage);
                         Stopwatch sw = new Stopwatch();
                         sw.Start();
-                        if (_config!.ConnectFrontEnd!.Equals("naifu", StringComparison.OrdinalIgnoreCase))
-                            GetNaifuImageBytes(prompts).ContinueWith(CallBack);
-                        else if (_config.ConnectFrontEnd.Equals("webui", StringComparison.OrdinalIgnoreCase))
-                            GetWebUIImageBytes(prompts).ContinueWith(CallBack);
+                        try
+                        {
+                            if (_config!.ConnectFrontEnd!.Equals("naifu", StringComparison.OrdinalIgnoreCase))
+                                GetNaifuImageBytes(prompts).ContinueWith(CallBack);
+                            else if (_config.ConnectFrontEnd.Equals("customwebui", StringComparison.OrdinalIgnoreCase))
+                                GetCustomWebUIImageBytes(prompts).ContinueWith(CallBack);
+                            else if (_config.ConnectFrontEnd.Equals("webui", StringComparison.OrdinalIgnoreCase))
+                                GetWebUIImageBytes(prompts).ContinueWith(CallBack);
 
-                        void CallBack(Task<byte[]?> task)
+                            void CallBack(Task<byte[]?> task)
+                            {
+                                sw.Stop();
+                                if (task.IsFaulted || task.IsCanceled || task.Result is null)
+                                {
+                                    if (string.IsNullOrEmpty(_config!.DrawErrorMessage))
+                                        Response(_config!.DrawErrorMessage!.Replace("<耗时>", (sw.ElapsedMilliseconds / 1000f).ToString()));
+                                    return;
+                                }
+                                GreenOnionsMessages msgs = new GreenOnionsImageMessage(new MemoryStream(task.Result));
+                                msgs.RevokeTime = _config.RevokeSecond;
+                                Response(msgs);
+                                if (!string.IsNullOrWhiteSpace(_config!.DrawEndMessage))
+                                    Response(_config.DrawEndMessage.Replace("<耗时>", (sw.ElapsedMilliseconds / 1000f).ToString()));
+                            }
+                        }
+                        catch (Exception ex)
                         {
                             sw.Stop();
-                            if (task.IsFaulted || task.IsCanceled || task.Result == null)
-                            {
-                                if (string.IsNullOrEmpty(_config!.DrawErrorMessage))
-                                    Response(_config!.DrawErrorMessage!.Replace("<耗时>", (sw.ElapsedMilliseconds / 1000f).ToString()));
-                                return;
-                            }
-                            GreenOnionsMessages msgs = new GreenOnionsImageMessage(new MemoryStream(task.Result));
-                            msgs.RevokeTime = _config.RevokeSecond;
-                            Response(msgs);
-                            if (!string.IsNullOrWhiteSpace(_config!.DrawEndMessage))
-                                Response(_config.DrawEndMessage.Replace("<耗时>", (sw.ElapsedMilliseconds / 1000f).ToString()));
+                            if (string.IsNullOrEmpty(_config!.DrawErrorMessage))
+                                Response(_config!.DrawErrorMessage!.Replace("<耗时>", (sw.ElapsedMilliseconds / 1000f).ToString()));
+
+                            for (int i = 0; i < _botConfig!.AdminQQ.Count; i++)
+                                _api.SendFriendMessageAsync(_botConfig.AdminQQ.ToArray()[i], $"葱葱NovelAi画图插件发生异常，{ex.Message}");
                         }
                         return true;
                     }
-
                 }
             }
             return false;
@@ -136,64 +133,28 @@ namespace GreenOnions.NovelAiClient
 
         private Task<byte[]?> GetWebUIImageBytes(string prompts)
         {
-            WebUIClient webuiClient = new WebUIClient(_config!.URL!);
+            WebUIClient webuiClient = new WebUIClient(_config!.fn_index, _config!.URL!);
             string defalutPrompt = _config!.DefaultPrompt!.Trim();
             if (!defalutPrompt.EndsWith(','))
                 defalutPrompt += ',';
             return webuiClient.PostAsync(defalutPrompt + prompts, _config!.DefaultUndesired, _config.ImageWidth, _config.ImageHeight);
         }
 
+        private Task<byte[]?> GetCustomWebUIImageBytes(string prompts)
+        {
+            CustomWebUIClient webuiClient = new CustomWebUIClient(_config!.fn_index, _config!.URL!);
+            string defalutPrompt = _config!.DefaultPrompt!.Trim();
+            if (!defalutPrompt.EndsWith(','))
+                defalutPrompt += ',';
+
+            string customDataPath = Path.Combine(_pluginPath!, "custom_data.txt");
+            string customData = File.ReadAllText(customDataPath);
+            return webuiClient.PostAsync(customData, defalutPrompt + prompts, _config!.DefaultUndesired ?? "");
+        }
+
         public bool WindowSetting()
         {
             return false;
         }
-    }
-
-    public class Config
-    {
-        /// <summary>
-        /// 命令
-        /// </summary>
-        public string? Cmd { get; set; }
-        /// <summary>
-        /// 连接的前端（Naifu/WebUI）
-        /// </summary>
-        public string? ConnectFrontEnd { get; set; }
-        /// <summary>
-        /// 前端地址
-        /// </summary>
-        public string? URL { get; set; }
-        /// <summary>
-        /// 图片宽
-        /// </summary>
-        public int ImageWidth { get; set; }
-        /// <summary>
-        /// 图片高
-        /// </summary>
-        public int ImageHeight { get; set; }
-        /// <summary>
-        /// 默认添加提示词
-        /// </summary>
-        public string? DefaultPrompt { get; set; }
-        /// <summary>
-        /// 默认屏蔽词
-        /// </summary>
-        public string? DefaultUndesired { get; set; }
-        /// <summary>
-        /// 开始绘制提示
-        /// </summary>
-        public string? StartDrawMessage{ get; set; }
-        /// <summary>
-        /// 绘制完成提示
-        /// </summary>
-        public string? DrawEndMessage { get; set; }
-        /// <summary>
-        /// 绘制错误提示
-        /// </summary>
-        public string? DrawErrorMessage { get; set; }
-        /// <summary>
-        /// 撤回时间（秒）
-        /// </summary>
-        public int RevokeSecond { get; set; }
     }
 }
