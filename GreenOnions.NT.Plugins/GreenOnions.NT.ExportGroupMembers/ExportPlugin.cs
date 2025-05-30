268a04e4c6745171408fabc7ccab081d046b0e3a
Alex1911-Jiang <tjxx_jgd@163.com>
2025-05-30 11:36:40 +0800

    登录后立刻导出

--- GreenOnions.NT.Plugins/GreenOnions.NT.ExportGroupMembers/ExportPlugin.cs
+++ GreenOnions.NT.Plugins/GreenOnions.NT.ExportGroupMembers/ExportPlugin.cs
@@ -20,6 +20,12 @@ namespace GreenOnions.NT.ExportGroupMembers
         public void OnLoaded(string pluginPath, BotContext bot, ICommonConfig commonConfig)
         {
             bot.Invoker.OnFriendMessageReceived += Invoker_OnFriendMessageReceived;
+            bot.Invoker.OnBotOnlineEvent += Invoker_OnBotOnlineEvent;
+        }
+
+        private async void Invoker_OnBotOnlineEvent(BotContext context, Lagrange.Core.Event.EventArg.BotOnlineEvent e)
+        {
+            await Export(context);
         }
 
         private async void Invoker_OnFriendMessageReceived(BotContext context, Lagrange.Core.Event.EventArg.FriendMessageEvent e)
@@ -45,34 +51,40 @@ namespace GreenOnions.NT.ExportGroupMembers
 
                 LogHelper.LogMessage($"开始导出机器人的群员信息给：{e.Chain.FriendUin}");
 
-                DataSet ds = new DataSet();
-                var groups = await context.FetchGroups();
-                foreach (var group in groups)
-                {
-                    LogHelper.LogMessage($"写入Sheet：{group.GroupName}-{group.GroupUin}");
-                    DataTable dt = new DataTable($"{group.GroupName}-{group.GroupUin}");
-                    dt.Columns.Add("QQ号");
-                    dt.Columns.Add("昵称");
-                    dt.Columns.Add("身份");
-                    dt.Columns.Add("等级");
-                    ds.Tables.Add(dt);
-                    var members = await context.FetchMembers(group.GroupUin);
-                    foreach (var member in members)
-                        dt.Rows.Add(member.Uin, member.MemberName, member.Permission, member.GroupLevel);
-                }
-
-                using MemoryStream ms = new MemoryStream();
-                ds.WriteExcelXml(ms);
-                string dir = Path.Combine(AppContext.BaseDirectory, "groups.xml");
-                byte[] xmlByte = ms.ToArray();
-                File.WriteAllBytes(dir, xmlByte);
-                LogHelper.LogMessage($"导出完成");
+                byte[] xmlByte = await Export(context);
                 await context.UploadFriendFile(e.Chain.FriendUin, new FileEntity(xmlByte, "groups.xml"));
             }
             catch (Exception ex)
             {
                 LogHelper.LogException(ex, $"{Name}插件发生了不在预见范围内的异常，错误信息：{ex.Message}");
             }
+        }
+
+        private async Task<byte[]> Export(BotContext context)
+        {
+            LogHelper.LogMessage($"开始导出群成员");
+            DataSet ds = new DataSet();
+            var groups = await context.FetchGroups();
+            foreach (var group in groups)
+            {
+                LogHelper.LogMessage($"写入Sheet：{group.GroupName}-{group.GroupUin}");
+                DataTable dt = new DataTable($"{group.GroupName}-{group.GroupUin}");
+                dt.Columns.Add("QQ号");
+                dt.Columns.Add("昵称");
+                dt.Columns.Add("身份");
+                ds.Tables.Add(dt);
+                var members = await context.FetchMembers(group.GroupUin);
+                foreach (var member in members)
+                    dt.Rows.Add(member.Uin, member.MemberName, member.Permission);
+            }
+
+            using MemoryStream ms = new MemoryStream();
+            ds.WriteExcelXml(ms);
+            string dir = Path.Combine(AppContext.BaseDirectory, "groups.xml");
+            byte[] xmlByte = ms.ToArray();
+            File.WriteAllBytes(dir, xmlByte);
+            LogHelper.LogMessage($"导出完成");
+            return xmlByte;
         }
     }
 }
